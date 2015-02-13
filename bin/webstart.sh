545e54aed15cddf1a1d86130b7b368b87eee53ac
Fabrizio Benigni <fbenigni@palantir.com>
2015-02-13 13:46:29 +0000

    Script to launch webstart given a domain.

---
+++ bin/webstart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOMAIN="$1"
+JNLP_URL="https://$DOMAIN:3280/webstart/palantir.jnlp?memory=CUSTOM&memcustom=256-1024"
+
+PATH_TO_THIS_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+COMMAND="dl.sh"
+
+"$PATH_TO_THIS_SCRIPT/$COMMAND" "$JNLP_URL" "$HOME/Downloads/webstarts" open
