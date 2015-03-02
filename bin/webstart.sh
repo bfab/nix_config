a6ecc0f0de9c43f3163af308e1199930c6d80eba
Fabrizio Benigni <fbenigni@palantir.com>
2015-03-02 23:07:30 +0000

    webstart.sh now also accepts an URL.

--- bin/webstart.sh
+++ bin/webstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOMAIN="$1"
+DOMAIN=$(sed -E 's|(https?://)?([^/:]*).*|\2|' <<< $1)
 JNLP_URL="https://$DOMAIN:3280/webstart/palantir.jnlp?memory=CUSTOM&memcustom=256-1024"
 
 PATH_TO_THIS_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
