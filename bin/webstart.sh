e11b3d350255d84b5e2ea5e34562db3209c78146
Fabrizio Benigni <fbenigni@palantir.com>
2016-08-24 10:18:04 +0000

    Create webstarts folder if absent; tidy up.

--- bin/webstart.sh
+++ bin/webstart.sh
@@ -2,8 +2,10 @@
 
 DOMAIN=$(sed -E 's|(https?://)?([^/:]*).*|\2|' <<< $1)
 JNLP_URL="https://$DOMAIN:3280/webstart/palantir.jnlp?memory=CUSTOM&memcustom=256-1024"
+JNLP_LOCAL_FOLDER="$HOME/Downloads/webstarts"
 
 PATH_TO_THIS_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-COMMAND="dl.sh"
 
-"$PATH_TO_THIS_SCRIPT/$COMMAND" "$JNLP_URL" "$HOME/Downloads/webstarts" open
+[ -d  "$JNLP_LOCAL_FOLDER" ] || mkdir -p "$JNLP_LOCAL_FOLDER"
+
+"$PATH_TO_THIS_SCRIPT/dl.sh" "$JNLP_URL" "$JNLP_LOCAL_FOLDER" open
