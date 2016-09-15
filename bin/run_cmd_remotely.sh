1abd58e552e1d0b544920f0587829ebcbe461ad7
Fabrizio Benigni <fbenigni@palantir.com>
2016-09-15 14:21:17 +0000

    Tidy up remote execution scripts.

--- bin/run_cmd_remotely.sh
+++ bin/run_cmd_remotely.sh
@@ -3,26 +3,28 @@ set -e
 
 
 print_usage_and_exit() {
-    echo -e "\nUsage: `basename $0` \"<user_1>@<host_1> ... <user_n>@<host_n>\" <script_full_path>"
+    echo -e "\nUsage: `basename $0` \"<user_1>@<host_1> ... <user_n>@<host_n>\" <command_to_run> [args]"
     exit 1
 }
 
 parse_params() {
-    [ $# = 2 ] || print_usage_and_exit
+    [ $# -gt 1 ] || print_usage_and_exit
 
     CMD_TARGETS=$1
-    COMMAND="$2"
+    shift
+    COMMAND=$@
 }
 
 run_command_on_hosts() {
 
     for TARGET in $CMD_TARGETS ;do
     	echo "\n> Running\n> \n>   $COMMAND\n> \n> as $TARGET ; you may be asked for a password...\n" 
-        ssh $TARGET /bin/bash -s <"$COMMAND"
+        ssh $TARGET "$COMMAND"
     done
 
     echo "\n> All done!"
 }
+
 #note: quoting here is important to avoid parameters being split on spaces
 parse_params "$@"
 
