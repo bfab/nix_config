ffc463108e094eb2d0ea8048130c08e1b5164513
Fabrizio Benigni <fbenigni@palantir.com>
2017-12-12 10:37:57 +0000

    Update ag install script.

--- notes/note_cmd_install_ag_silver_searcher.sh
+++ notes/note_cmd_install_ag_silver_searcher.sh
@@ -4,18 +4,20 @@ set -e
 
 [ `id -u` == 0 ] || { echo "Must run as root" ; exit 1; }
 
-yum install -y pcre-devel
-yum install -y xz-devel
-yum install -y git
-yum install -y gcc
-yum install -y automake
+# yum install -y pcre-devel
+# yum install -y xz-devel
+# yum install -y git
+# yum install -y gcc
+# yum install -y automake
 
-pushd /usr/local/src
-git clone https://github.com/ggreer/the_silver_searcher.git
-pushd the_silver_searcher
-./build.sh
-make install
-popd
-popd
+# pushd /usr/local/src
+# git clone https://github.com/ggreer/the_silver_searcher.git
+# pushd the_silver_searcher
+# ./build.sh
+# make install
+# popd
+# popd
+
+yum install the_silver_searcher -y
 
 which ag >/dev/null && echo "Installed." || echo "Something went wrong; not installed."
