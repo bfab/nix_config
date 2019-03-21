938df6d943236d30387566c7e3a513d8e0d68653
Fabrizio Benigni <fbenigni@palantir.com>
2019-03-21 14:15:47 +0000

    Increase webstart max heap to 2GB.

--- bin/webstart.sh
+++ bin/webstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOMAIN=$(sed -E 's|(https?://)?([^/:]*).*|\2|' <<< $1)
-JNLP_URL="https://$DOMAIN:3280/webstart/palantir.jnlp?memory=CUSTOM&memcustom=256-1024"
+JNLP_URL="https://$DOMAIN:3280/webstart/palantir.jnlp?memory=CUSTOM&memcustom=256-2048"
 JNLP_LOCAL_FOLDER="$HOME/Downloads/webstarts"
 
 PATH_TO_THIS_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
